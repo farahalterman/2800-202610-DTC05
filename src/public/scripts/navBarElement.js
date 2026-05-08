1d57a1548105bb28a485a93e7d325b57864fb70f
Lyra <d.g.shorman@my.bcit.ca>
2026-05-08 12:13:14 -0700

    put navbar HTML into custom tag

--- src/public/scripts/navBarElement.js
+++ src/public/scripts/navBarElement.js
@@ -8,29 +8,27 @@ connectedCallback() {
             currentPage === page ? "bg-orange-200 scale-110 font-bold" : "";
 
         this.innerHTML = `
-        <footer>
-            <div>
-
-                <a href="index.html">
-                    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-bottom">
+            <div class="container-fluid d-flex justify-content-around" id="navBar">
+                <div id="navButton">
+                    <a href="index.html">
                         <h1>1</h1>
-                    </div>
-                </a>
-
-                <a href="index.html">
-                    <div>
-                        <h1>2</h1>
-                    </div>
-                </a>
-
-                <a href="index.html">
-                    <div>
-                        <h1>3</h1>
-                    </div>
-                </a>
-
+                    </a>
+                </div>
+    
+                <div id="navButton">
+                    <a href="index.html">
+                        <h1>1</h1>
+                    </a>
+                </div>
+    
+                <div id="navButton">
+                    <a href="index.html">
+                        <h1>1</h1>
+                    </a>
+                </div>
             </div>
-        </footer>
+        </nav>
         `;
     }
 }
