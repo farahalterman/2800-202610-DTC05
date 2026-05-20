a924ece45bb553dc51238b22b34907d86a013a55
farahalterman <108204840+farahalterman@users.noreply.github.com>
2026-05-19 22:33:57 -0700

    Add password field to user table

--- init.sql
+++ init.sql
@@ -15,6 +15,7 @@ CREATE TABLE "User" (
     user_id SERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     home_location VARCHAR(255),
     admin BOOLEAN DEFAULT false NOT NULL,
