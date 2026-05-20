a4f7ce91fa5d13d18f229a42edfcaf91ee8f6d9c
farahalterman <108204840+farahalterman@users.noreply.github.com>
2026-05-19 22:27:36 -0700

    Add initial middleware to the server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors"); // Will need if we host front and back end on different ports
 
-app.use(express.static('src'));
+// Middleware
+app.use(express.static("src"));
+app.use(express.urlencoded());
+app.use(cors());
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log("Server running on port 3000"));
