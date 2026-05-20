5a2f180187c5d593f1dd816535236f54ae907372
armin <ahadjifaradji@my.bcit.ca>
2026-05-20 15:31:01 -0700

    Add a login html page.

--- src/scripts/navBarElement.js
+++ src/scripts/navBarElement.js
@@ -1,38 +1,36 @@
-
-
 class navBar extends HTMLElement {
-connectedCallback() {
-        const currentPage = window.location.pathname.split("/").pop(); 
+  connectedCallback() {
+    const currentPage = window.location.pathname.split("/").pop();
 
-        const isActive = (page) =>
-            currentPage === page ? "bg-orange-200 scale-110 font-bold" : "";
+    const isActive = (page) =>
+      currentPage === page ? "bg-orange-200 scale-110 font-bold" : "";
 
-        this.innerHTML = `
-        <footer>
-            <div class="container" id="navbar">
+    // Check if user is logged in
+    const token = localStorage.getItem("token");
+    const user = token ? JSON.parse(localStorage.getItem("user") || "null") : null;
 
-                <div class="navItem">
-                    <a href="index.html">
-                        <h1>HOME</h1>
-                    </a>
-                </div>
+    const accountLink = user
+      ? `<a href="account.html" class="navItem">
+           <div><h1>${user.first_name.toUpperCase()}</h1></div>
+         </a>`
+      : `<a href="login.html" class="navItem">
+           <div><h1>LOGIN</h1></div>
+         </a>`;
 
-                <div class="navItem">
-                    <a href="index.html">
-                        <h1>FAVOURITES</h1>
-                    </a>
-                </div>
-
-                <div class="navItem">
-                    <a href="index.html">
-                        <h1>ACCOUNT</h1>
-                    </a>
-                </div>
-
-            </div>
-        </footer>
-        `;
-    }
+    this.innerHTML = `
+      <footer>
+        <div class="container" id="navbar">
+          <a href="home.html" class="navItem">
+            <div><h1>HOME</h1></div>
+          </a>
+          <a href="index.html" class="navItem">
+            <div><h1>FAVORITES</h1></div>
+          </a>
+          ${accountLink}
+        </div>
+      </footer>
+    `;
+  }
 }
 
-customElements.define('nav-bar', navBar);
+customElements.define("nav-bar", navBar);
